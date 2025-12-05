f864a669509adb40de70f73845f9f8353101521f
imkabir54 <kkabir9900@gmail.com>
2025-12-05 20:52:13 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import HeroBanner from '../../components/HomeComponents/HeroBanner/HeroBanner'
+
+const Home = () => {
+  return (
+    <div>
+      <HeroBanner></HeroBanner>
+    </div>
+  )
+}
+
+export default Home
